9ac4b1fd0bbc8c0a678585ff76847e40dc82dd1a
Raj Natarajan <rajday19@gmail.com>
2021-08-09 20:02:48 -0700

    Adding 3 epochs for training

--- DeepLearning4coders/l1.py
+++ DeepLearning4coders/l1.py
@@ -12,4 +12,5 @@ def is_cat(x):
 dls = ImageDataLoaders.from_name_func(path, get_image_files(path), valid_pct=0.2, seed=42, label_func=is_cat, item_tfms=Resize(224))
 
 learn = cnn_learner(dls, resnet34, metrics=error_rate)
-learn.fine_tune(1)
+learn.model = model.cuda()
+learn.fine_tune(3)
